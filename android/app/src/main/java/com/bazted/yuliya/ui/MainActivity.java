980ac01bac37635c012636714810d16d083b22d9
Taras Bazyshyn <bazushun.taras@gmail.com>
2015-05-29 23:30:34 +0200

    possibility to log out from main activity

--- android/app/src/main/java/com/bazted/yuliya/ui/MainActivity.java
+++ android/app/src/main/java/com/bazted/yuliya/ui/MainActivity.java
@@ -8,13 +8,22 @@ import android.widget.TextView;
 
 import com.bazted.yuliya.R;
 import com.bazted.yuliya.app.BaseActivity;
+import com.bazted.yuliya.app.YApp;
+import com.bazted.yuliya.ui.login.LoginActivity_;
 
 import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.App;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.OptionsItem;
+import org.androidannotations.annotations.OptionsMenu;
 import org.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.activity_main)
+@OptionsMenu({R.menu.menu_main})
 public class MainActivity extends BaseActivity {
+
+    @App
+    YApp app;
 
     @ViewById(R.id.hello_tv)
     TextView textView;
@@ -22,5 +31,12 @@ public class MainActivity extends BaseActivity {
     @AfterViews
     void start() {
         textView.setText(R.string.app_name);
+    }
+
+    @OptionsItem(R.id.action_logout)
+    void logoutOptionSelected() {
+        app.auth().logout();
+        LoginActivity_.intent(this).start();
+        finish();
     }
 }
