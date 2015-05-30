c3f71cd51dd83351926cd9953293e583da4557de
Taras Bazyshyn <bazushun.taras@gmail.com>
2015-05-30 09:10:19 +0200

    working nfc listener in open app

--- android/app/src/main/java/com/bazted/yuliya/ui/MainActivity.java
+++ android/app/src/main/java/com/bazted/yuliya/ui/MainActivity.java
@@ -4,9 +4,13 @@
 
 package com.bazted.yuliya.ui;
 
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
+import android.os.Bundle;
 import android.os.Parcelable;
 import android.util.Log;
 import android.widget.TextView;
@@ -35,6 +39,9 @@ public class MainActivity extends BaseActivity {
 
     @ViewById(R.id.hello_tv)
     TextView textView;
+    private NfcAdapter nfcAdapter;
+    private PendingIntent pendingIntent;
+    private IntentFilter[] intentFilters;
 
     @AfterViews
     void start() {
@@ -49,19 +56,53 @@ public class MainActivity extends BaseActivity {
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
-        // Check to see that the Activity started due to an Android Beam
-        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(getIntent().getAction())) {
-            processIntent(getIntent());
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (nfcAdapter == null) {
+            Log.e(LOG, "no nfc");
+            return;
+        } else {
+            Log.e(LOG, "we have nfc");
         }
+
+        pendingIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, getClass()).setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),
+                0);
+
+        intentFilters = new IntentFilter[3];
+        intentFilters[0] = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        intentFilters[1] = new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED);
+        intentFilters[2] = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+
     }
 
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
-        Log.e(LOG, "new Intent" + intent.toString());
-        setIntent(intent);
+        Log.e(LOG, "onNewIntent|" + intent.toString());
+        String action = intent.getAction();
+        if (action != null) {
+            processIntent(intent);
+            Log.e(LOG, action);
+
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (nfcAdapter != null) {
+            nfcAdapter.enableForegroundDispatch(this, pendingIntent, intentFilters, null);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (nfcAdapter != null) {
+            nfcAdapter.disableForegroundDispatch(this);
+        }
     }
 
     /**
@@ -72,14 +113,13 @@ public class MainActivity extends BaseActivity {
             Log.e(LOG, "intent is null");
             return;
         }
-        if (intent.getAction() != null) {
-            Log.e("LOG", intent.getAction());
-        }
+        Log.e(LOG, intent.toString());
         Parcelable[] rawMsgs = intent.getParcelableArrayExtra(
                 NfcAdapter.EXTRA_NDEF_MESSAGES);
         if (rawMsgs != null) {
             Parcelable rawMsg;
             NdefMessage msg;
+            NdefRecord ndefRecord;
             String payload;
             String jsonBody = null;
             //noinspection ForLoopReplaceableByForEach
@@ -89,7 +129,9 @@ public class MainActivity extends BaseActivity {
                 //noinspection ForLoopReplaceableByForEach
                 int length = msg.getRecords().length;
                 for (int j = 0; j < length; j++) {
-                    payload = new String(msg.getRecords()[j].getPayload());
+                    ndefRecord = msg.getRecords()[j];
+                    payload = new String(ndefRecord.getPayload());
+                    Log.e(LOG, payload);
                     switch (j) {
                         case 1:
                             jsonBody = payload;
@@ -107,4 +149,5 @@ public class MainActivity extends BaseActivity {
             showToast("Sorry TAG is not readable retry");
         }
     }
+
 }
